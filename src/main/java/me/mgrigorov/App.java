a317f28cc3d49f99e8d9131a7cd13463f6b6437f
Mikhail Grigorov <mgrigorov@productengine.com>
2015-11-29 17:20:32 -0500

    minor code update

--- src/main/java/me/mgrigorov/App.java
+++ src/main/java/me/mgrigorov/App.java
@@ -3,12 +3,30 @@ package me.mgrigorov;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class App {
-    private static final Logger logger = LogManager.getRootLogger();
+    private static final int MIN_VALUE = -100000;
+    private static final int MAX_VALUE = 100000;
+    private static final int DELAY = 300;
+    private static final int PERIOD = 500;
+    private static final Logger LOGGER = LogManager.getRootLogger();
 
-    public static void main(String[] args) {
-        logger.info("HELLO FLUME!");
+    public static void main(String[] args) throws IOException {
+        final Timer timer = new Timer();
 
-        System.out.println("DONE!");
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                LOGGER.info(new Random().nextInt(MAX_VALUE - MIN_VALUE + 1) + MIN_VALUE); // Random int from -100000 to 100000
+            }
+        }, DELAY, PERIOD);
+
+        System.out.println("press enter to exit...");
+        System.in.read();
+        System.exit(0);
     }
 }
